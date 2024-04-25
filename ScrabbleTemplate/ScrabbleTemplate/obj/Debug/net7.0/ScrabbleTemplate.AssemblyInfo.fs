52f43ea2abbc7ca5f005bd2e34bd9c8d10f7e69b
RazzerTazzer <soeholt98@gmail.com>
2024-04-25 09:08:19 +0200

    test to see if git works

--- ScrabbleTemplate/ScrabbleTemplate/obj/Debug/net7.0/ScrabbleTemplate.AssemblyInfo.fs
+++ ScrabbleTemplate/ScrabbleTemplate/obj/Debug/net7.0/ScrabbleTemplate.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b089592b9b4718c68a39d10873ef14cc368bb0")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
